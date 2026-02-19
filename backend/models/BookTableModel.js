95e063c538dffefd1d082368a7ee8ef5a8acacf1
dhanasai2 <saigundumogula5@gmail.com>
2026-02-19 18:18:45 +0530

    [AI-AGENT] Fix LINTING error in backend\models\BookTableModel.js: Removed console output

--- backend/models/BookTableModel.js
+++ backend/models/BookTableModel.js
@@ -5,7 +5,7 @@ import db from "../config/database.js";
 export const insertBooking = (data,result) => {
     db.query("INSERT INTO booktable SET ?",data, (err,results)=> {
         if (err){
-            console.log(err);
+            // console.log(err);
             result(err,null);
         }else{
             result(null,results[0]);
