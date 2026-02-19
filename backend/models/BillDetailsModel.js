3cd54bb78d8fb2ff5add2dab21e7775ccd7b5084
dhanasai2 <saigundumogula5@gmail.com>
2026-02-19 18:17:54 +0530

    [AI-AGENT] Fix LINTING error in backend\models\BillDetailsModel.js: Removed console output

--- backend/models/BillDetailsModel.js
+++ backend/models/BillDetailsModel.js
@@ -5,7 +5,7 @@ import db from "../config/database.js";
 export const insertBillDetails = (data,result) => {
     db.query("INSERT INTO billdetails SET ?",data, (err,results)=> {
         if (err){
-            console.log(err);
+            // console.log(err);
             result(err,null);
         }else{
             result(null,results[0]);
@@ -18,7 +18,7 @@ export const insertBillDetails = (data,result) => {
 export const getBillDetails = (id,result) => {
     db.query("SELECT * FROM billdetails WHERE bill_id = ?",id, (err,results)=> {
         if (err){
-            console.log(err);
+// console.log(err);
             result(err,null);
         }else{
             result(null,results);
