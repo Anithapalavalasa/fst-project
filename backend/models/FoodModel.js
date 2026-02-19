d5039cd0904cbde6b7312913298a3a7de7754e60
CI-Agent <ciagent@rift.ai>
2026-02-19 18:19:29 +0530

    [AI-AGENT] Fix LINTING error in backend\models\FoodModel.js: Removed console.log statements

--- backend/models/FoodModel.js
+++ backend/models/FoodModel.js
@@ -5,7 +5,7 @@ import db from "../config/database.js";
 export const getFoods = (result) => {
     db.query("SELECT * FROM food", (err,results)=> {
         if (err){
-            console.log(err);
+            result(err,null);
             result(err,null);
         }else{
             result(null,results);
@@ -15,9 +15,9 @@ export const getFoods = (result) => {
 
 // get single Foods
 export const getFoodById = (id,result) => {
-    db.query("SELECT * FROM food WHERE food_id = ?",[id], (err,results)=> {
+            result(err,null);
         if (err){
-            console.log(err);
+
             result(err,null);
         }else{
             result(null,results[0]);
@@ -25,36 +25,36 @@ export const getFoodById = (id,result) => {
     });
 };
 
-// insert Food
+            result(err,null);
 export const insertFood = (data,result) => {
-    db.query("INSERT INTO food SET ?",data, (err,results)=> {
         if (err){
-            console.log(err);
+            result(err,null);
+
             result(err,null);
         }else{
             result(null,results[0]);
         }
     });
-};
+            result(err,null);
 
 // update Food
 export const updateFoodById = (data,id,result) => {
     db.query("UPDATE food SET food_name = ?, food_price = ? WHERE food_id = ?",[data.food_name, data.food_price, id], (err,results)=> {
         if (err){
-            console.log(err);
-            result(err,null);
+
+
         }else{
             result(null,results);
-        }
+
     });
 };
 
 
 // delete Food
-export const deleteFoodById = (id,result) => {
-    db.query("DELETE FROM food WHERE food_id = ?",[id], (err,results)=> {
+
+            result(err,null);
         if (err){
-            console.log(err);
+            // console.log(err);
             result(err,null);
         }else{
             result(null,results);
